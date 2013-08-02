b14cdf6cb192f0e49d1ed15d398b9c87afb13735
icedream <icedream2k9@die-optimisten.net>
2013-08-02 11:44:15 +0200

    Fixing up lua build script.

--- build-lua.bat
+++ build-lua.bat
@@ -1,31 +1,45 @@
 @echo off
-echo Building lua library...
-
 set solutiondir=%cd%
 
 set platform=%1
 if "%platform%"=="" set platform=x86
+if "%platform%"=="Any CPU" set platform=x86
+if "%platform%"=="AnyCPU" set platform=x86
+
 set platform_clr=%platform%
 if "%platform_clr%"=="x86" set platform_clr=Win32
 
 set configuration=%2
 if "%configuration%"=="" set configuration=Release
 
+set configuration_clr=%configuration%
+if "%configuration_clr%"=="DebugKopiLua" set configuration_clr=Debug
+if "%configuration_clr%"=="ReleaseKopiLua" set configuration_clr=Release
+
 set target=%3
-if "%target%"=="" set target=%solutiondir%\bin\%platform%\%configuration%\
+if %target%=="" set target="%solutiondir%\bin\%platform%\%configuration%\"
 
 mkdir externals >NUL 2>NUL
 pushd externals
 mkdir lua >NUL 2>NUL
 pushd lua
 
-echo Compiling for "%configuration%|%platform%"
 
-cmake ../../externals/NLua/Core/KeraLua/external/lua
-cmake --build . --config "%configuration%" --target clean -- /projectconfig "%configuration%|%platform_clr%"
-cmake --build . --config "%configuration%" -- /projectconfig "%configuration%|%platform_clr%"
-
-copy /y "bin\*.dll" "%target%"
+del %target%\lua*.dll
+::if not exist %target%\KopiLua.dll (
+	echo ** KopiLua not found in target.
+	echo ** Compiling Lua DLL for "%configuration%|%platform%"
+	cmake ../../externals/NLua/Core/KeraLua/external/lua
+	::cmake --build . --config "%configuration%" --target clean -- /projectconfig "%configuration_clr%|%platform_clr%"
+	cmake --build . --config "%configuration%" -- /projectconfig "%configuration_clr%|%platform_clr%"
+	echo ** Copying Lua DLL to %target%...
+	copy /y "bin\lua*.dll" %target%
+::) else (
+::	echo ** KopiLua found in target.
+::	echo ** Not compiling Lua DLL for "%configuration%|%platform%"
+::)
 
 popd
 popd
+
+echo ** Lua Setup done.
