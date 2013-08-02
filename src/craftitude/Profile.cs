851898191900c3a2ae7c02e657924ae8091d5f33
icedream <icedream2k9@die-optimisten.net>
2013-08-03 00:34:39 +0200

    profile.bson for saving profile information.

--- src/craftitude/Profile.cs
+++ src/craftitude/Profile.cs
@@ -16,10 +16,30 @@ namespace Craftitude
     public class Profile
     {
         DirectoryInfo _directory;
+        List<Tuple<PackageAction, Package>> _pendingPackages = new List<Tuple<PackageAction, Package>>();
+
+        FileInfo _bsonFile;
 
         public Profile(DirectoryInfo profileDirectory)
         {
             _directory = profileDirectory;
+            _bsonFile = profileDirectory.GetFile("profile.bson");
+
+            if (!_bsonFile.Exists)
+            {
+                ProfileInfo = new ProfileInfo();
+            }
+            else
+            {
+                using (var bsonStream = _bsonFile.Open(FileMode.OpenOrCreate))
+                {
+                    using (var bsonReader = new BsonReader(bsonStream))
+                    {
+                        var jsonSerializer = new JsonSerializer();
+                        ProfileInfo = jsonSerializer.Deserialize<ProfileInfo>(bsonReader);
+                    }
+                }
+            }
         }
 
         public ProfileInfo ProfileInfo { get; private set; }
@@ -27,6 +47,90 @@ namespace Craftitude
         public DirectoryInfo Directory { get { return _directory; } }
 
         public string Path { get { return _directory.ToString(); } }
+
+        public void AppendPackage(Package package, PackageAction action)
+        {
+            if (_pendingPackages.Any(p => p.Item2 == package))
+            {
+                DependPackage(package);
+            }
+            
+            _pendingPackages.Add(new Tuple<PackageAction, Package>(action, package));
+        }
+
+        public void DependPackage(Package package)
+        {
+            _pendingPackages.RemoveAll(p => p.Item2 == package);
+        }
+
+        public void RunTasks()
+        {
+            // Sort by action (uninstall/purge => install/update => configure)
+            _pendingPackages.Sort(new Comparison<Tuple<PackageAction, Package>>((a, b) => ((byte)a.Item1).CompareTo((byte)b.Item1)));
+
+            foreach (var item in _pendingPackages)
+            {
+                PackageAction action = item.Item1;
+                Package package = item.Item2;
+
+                switch (action)
+                {
+                    case PackageAction.Install:
+                        if (ProfileInfo.InstalledPackages.Any(p => p.Id == package.Metadata.Id))
+                            throw new InvalidOperationException(string.Format("Package {0} already installed.", item.Item2.Metadata.Id));
+
+                        Console.WriteLine("{2} {0} {1}...", package.Metadata.Id, package.Metadata.Version, "Installing");
+                        break;
+                    case PackageAction.Uninstall:
+                        if (!ProfileInfo.InstalledPackages.Any(p => p.Id == package.Metadata.Id))
+                            throw new InvalidOperationException(string.Format("Package {0} not installed.", item.Item2.Metadata.Id));
+
+                        Console.WriteLine("{2} {0} {1}...", package.Metadata.Id, package.Metadata.Version, "Uninstalling");
+                        break;
+                    case PackageAction.Purge:
+                        if (!ProfileInfo.InstalledPackages.Any(p => p.Id == package.Metadata.Id))
+                            throw new InvalidOperationException(string.Format("Package {0} not installed.", item.Item2.Metadata.Id));
+
+                        Console.WriteLine("{2} {0} {1}...", package.Metadata.Id, package.Metadata.Version, "Purging");
+                        break;
+                    case PackageAction.Update:
+                        if (!ProfileInfo.InstalledPackages.Any(p => p.Id == package.Metadata.Id))
+                            throw new InvalidOperationException(string.Format("Package {0} not installed.", item.Item2.Metadata.Id));
+
+                        Console.WriteLine("{2} {0} {1}...", package.Metadata.Id, package.Metadata.Version, "Updating");
+                        break;
+                    case PackageAction.Configure:
+                        if (!ProfileInfo.InstalledPackages.Any(p => p.Id == package.Metadata.Id))
+                            throw new InvalidOperationException(string.Format("Package {0} not installed yet, install first.", item.Item2.Metadata.Id));
+
+                        Console.WriteLine("{2} {0} {1}...", package.Metadata.Id, package.Metadata.Version, "Configuring");
+                        break;
+                }
+
+                package.RunTarget(this, action.ToString().ToLower());
+            }
+        }
+
+        public void Save()
+        {
+            using (var bsonStream = _bsonFile.Open(FileMode.OpenOrCreate))
+            {
+                using (var bsonWriter = new BsonWriter(bsonStream))
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(bsonWriter, this);
+                }
+            }
+        }
+    }
+
+    public enum PackageAction : byte
+    {
+        Uninstall = 1,
+        Purge = 2,
+        Install = 3,
+        Update = 4,
+        Configure = 5
     }
 
     [Serializable]
@@ -45,6 +149,7 @@ namespace Craftitude
             MinecraftVersion = string.Empty;
             MinecraftVersionType = "release";
             TweakClasses = new List<string>();
+            InstalledPackages = new List<PackageMetadata>();
             LastUpdateTime = DateTime.MinValue;
         }
 
@@ -87,6 +192,8 @@ namespace Craftitude
         public string MinecraftVersionType { get; set; }
 
         public List<string> TweakClasses { get; set; }
+
+        public List<PackageMetadata> InstalledPackages { get; set; }
 
         public DateTime LastUpdateTime { get; set; }
     }
