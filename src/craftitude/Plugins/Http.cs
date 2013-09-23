38eaba200d839a72177aaed023fecc0bf77e8f3d
icedream <icedream2k9@die-optimisten.net>
2013-09-23 11:58:57 +0200

    Custom headers for HTTP downloads (in LUA scripts).

--- src/craftitude/Plugins/Http.cs
+++ src/craftitude/Plugins/Http.cs
@@ -9,11 +9,29 @@ namespace Craftitude.Plugins
 {
     public class Http
     {
-        public static string Download(string remoteFile)
+        public static string Download(string remoteFile, Dictionary<string, string> customheaders = null)
         {
+            /*
+            var headers = new Dictionary<string, string>()
+            {
+                { "cache-control", "no-store,max-age=0,no-cache" },
+                { "expires", "0" },
+                { "pragma", "no-cache" }
+            };
+             */
+            var headers = new Dictionary<string, string>();
+            if (customheaders != null)
+                foreach (var item in customheaders)
+                {
+                    if (headers.ContainsKey(item.Key.ToLower()))
+                        headers[item.Key.ToLower()] = item.Value;
+                    else
+                        headers.Add(item.Key.ToLower(), item.Value);
+                }
             using (var wc = new WebClient())
             {
                 string localFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                wc.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
                 wc.DownloadFile(remoteFile, localFile);
                 return localFile;
             }
